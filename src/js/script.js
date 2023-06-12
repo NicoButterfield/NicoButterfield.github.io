acc33314e46a81f0d551d8e9eb0604ad5f3e8012
Nico Butterfield <nico@macbook-pro.myfiosgateway.com>
2023-06-12 11:43:41 -0400

    improved projects page

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,8 @@ function myGet(num)
         "../files/images/todos.jpg", 
         "../files/images/webDev3.jpg",
         "../files/images/scrap.jpg",
-        "../files/images/kmSS.png"
+        "../files/images/kmSS.png",
+        "../files/images/cap.jpg"
     ]
 
     const projLinks = 
@@ -22,6 +23,8 @@ function myGet(num)
         "https://nicobutterfield.github.io/HtmlCSSJavascipt/",
         "https://nicobutterfield.github.io/Scrapbook/",
         "https://nicobutterfield.github.io/DataViz-MovieGraph/kmeansGraph/index.html",
+        "https://nicobutterfield.github.io/Nico-Danny-Capstone/"
+        
     ]
 
     const descriptions = 
@@ -30,6 +33,7 @@ function myGet(num)
         "A responsive website that displays basic information describing HTML, CSS and Javascript.",
        "A website that uses html, css and javascript to display and flip through photos.",
        "An interactive, force directed graph that draws edges between movie nodes based on their shared actors and uses a k-means algorithm to create different colored, distance based groups, making the graph easily readable to viewers.",
+       "A recipe sharing platform that allows users to create new recipes, post recipes to their community, and view friends recipes in their feed.",
 
     ]
 
@@ -51,11 +55,13 @@ function myGet(num)
         document.getElementById("project1").classList.remove("active-nav-link");
         document.getElementById("project2").classList.remove("active-nav-link");
         document.getElementById("project3").classList.remove("active-nav-link");
+        document.getElementById("project4").classList.remove("active-nav-link");
 
         document.getElementById("project0").style.opacity = 1;
         document.getElementById("project1").style.opacity = 0.3;
         document.getElementById("project2").style.opacity = 0.3;
         document.getElementById("project3").style.opacity = 0.3;
+        document.getElementById("project4").style.opacity = 0.3;
     }
 
     if(num === 1)
@@ -64,11 +70,13 @@ function myGet(num)
         document.getElementById("project1").classList.add("active-nav-link");
         document.getElementById("project2").classList.remove("active-nav-link");
         document.getElementById("project3").classList.remove("active-nav-link");
+        document.getElementById("project4").classList.remove("active-nav-link");
     
         document.getElementById("project0").style.opacity = 0.3;
         document.getElementById("project1").style.opacity = 1;
         document.getElementById("project2").style.opacity = 0.3;
         document.getElementById("project3").style.opacity = 0.3;
+        document.getElementById("project4").style.opacity = 0.3;
     }
 
     if(num === 2)
@@ -78,11 +86,13 @@ function myGet(num)
         document.getElementById("project1").classList.remove("active-nav-link");
         document.getElementById("project2").classList.add("active-nav-link");
         document.getElementById("project3").classList.remove("active-nav-link");
+        document.getElementById("project4").classList.remove("active-nav-link");
         
         document.getElementById("project0").style.opacity = 0.3;
         document.getElementById("project1").style.opacity = 0.3;
         document.getElementById("project2").style.opacity = 1;
         document.getElementById("project3").style.opacity = 0.3;
+        document.getElementById("project4").style.opacity = 0.3;
     }
 
 
@@ -92,12 +102,28 @@ function myGet(num)
         document.getElementById("project1").classList.remove("active-nav-link");
         document.getElementById("project2").classList.remove("active-nav-link");
         document.getElementById("project3").classList.add("active-nav-link");
+        document.getElementById("project4").classList.remove("active-nav-link");
        
         document.getElementById("project0").style.opacity = 0.3;
         document.getElementById("project1").style.opacity = 0.3;
         document.getElementById("project2").style.opacity = 0.3;
         document.getElementById("project3").style.opacity = 1;
+        document.getElementById("project4").style.opacity = 0.3;
     }
     
+    if(num === 4)
+    {
+        document.getElementById("project0").classList.remove("active-nav-link");
+        document.getElementById("project1").classList.remove("active-nav-link");
+        document.getElementById("project2").classList.remove("active-nav-link");
+        document.getElementById("project3").classList.remove("active-nav-link");
+        document.getElementById("project4").classList.add("active-nav-link");
+       
+        document.getElementById("project0").style.opacity = 0.3;
+        document.getElementById("project1").style.opacity = 0.3;
+        document.getElementById("project2").style.opacity = 0.3;
+        document.getElementById("project3").style.opacity = 0.3;
+        document.getElementById("project4").style.opacity = 1;
+    }
 
 }
